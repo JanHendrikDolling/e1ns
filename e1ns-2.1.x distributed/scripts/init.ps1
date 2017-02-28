82ecdc3a706d5074e9b4f46825ac3d15937d8385
JanHendrikDolling <github@sp.dollares.de>
2017-02-28 10:51:22 +0100

    add init script

---
+++ e1ns-2.1.x distributed/scripts/init.ps1
@@ -0,0 +1,26 @@
+﻿$FolderName = "C:\share"
+$ShareName = "data"
+
+# create local folders
+it (!(TEST-PATH $FolderName)) { 
+    NEW-ITEM $FolderName -type Directory
+}
+$SetupFolderName = $FolderName + "\setup"
+New-Item -ItemType Directory -Force -Path $SetupFolderName
+
+# download setup files
+$scio_client = "https://www.hidrive.strato.com/wget/lCLuU2G7"
+$scio_server = "https://www.hidrive.strato.com/wget/UAru0rve"
+$scio_db = "https://www.hidrive.strato.com/wget/LBLuUBRG"
+$e1ns = "https://www.hidrive.strato.com/wget/s7LO0GH0" 
+$e1ns_config = "https://www.hidrive.strato.com/wget/HpLO0mAk"  
+
+Invoke-WebRequest -Method Get -Uri $scio_server -OutFile $SetupFolderName\setup_scio_server.exe ;
+Invoke-WebRequest -Method Get -Uri $scio_client -OutFile $SetupFolderName\setup_scio_client.exe ;
+Invoke-WebRequest -Method Get -Uri $scio_db -OutFile $SetupFolderName\db.zip ;
+Invoke-WebRequest -Method Get -Uri $e1ns -OutFile $SetupFolderName\setup_e1ns.exe ;
+Invoke-WebRequest -Method Get -Uri $e1ns_config -OutFile $SetupFolderName\e1ns_config.yml ;
+
+
+# create share
+New-SmbShare –Name $ShareName –Path $FolderName –FullAccess Everyone 
