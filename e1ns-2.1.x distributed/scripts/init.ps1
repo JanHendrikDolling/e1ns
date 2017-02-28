ded0badd46c55836fbc0f92cf31844bed9608be7
JanHendrikDolling <github@sp.dollares.de>
2017-02-28 17:28:43 +0100

    scripts

--- e1ns-2.1.x distributed/scripts/init.ps1
+++ e1ns-2.1.x distributed/scripts/init.ps1
@@ -21,14 +21,24 @@ $scio_client = "https://www.hidrive.strato.com/wget/lCLuU2G7"
 $scio_server = "https://www.hidrive.strato.com/wget/UAru0rve"
 $scio_db = "https://www.hidrive.strato.com/wget/LBLuUBRG"
 $e1ns = "https://www.hidrive.strato.com/wget/s7LO0GH0" 
-$e1ns_config = "https://www.hidrive.strato.com/wget/HpLO0mAk"  
+$e1ns_config = "https://www.hidrive.strato.com/wget/HpLO0mAk"
+$setup_script = "https://raw.githubusercontent.com/JanHendrikDolling/e1ns/master/e1ns-2.1.x%20distributed/scripts/setup.ps1"
 
 Invoke-WebRequest -Method Get -Uri $scio_server -OutFile $SetupFolderName\setup_scio_server.exe ;
 Invoke-WebRequest -Method Get -Uri $scio_client -OutFile $SetupFolderName\setup_scio_client.exe ;
 Invoke-WebRequest -Method Get -Uri $scio_db -OutFile $SetupFolderName\db.zip ;
 Invoke-WebRequest -Method Get -Uri $e1ns -OutFile $SetupFolderName\setup_e1ns.exe ;
 Invoke-WebRequest -Method Get -Uri $e1ns_config -OutFile $SetupFolderName\e1ns_config.yml ;
+Invoke-WebRequest -Method Get -Uri $setup_script -OutFile $SetupFolderName\setup.ps1 ;
 
 
 # create share
 New-SmbShare –Name $ShareName –Path $FolderName –FullAccess Everyone 
+
+$securepassword = ConvertTo-SecureString $adminPw -AsPlainText -Force
+$vmcred = new-object -typename System.Management.Automation.PSCredential -argumentlist $adminUser, $securepassword
+
+$scriptPath = "\\" + $vmDB + "\data\setup\setup.ps1"
+$argumentList = " -vmName " + $vmUi1 + " -vmDB " + $vmDB
+
+Invoke-Command -ComputerName $vmUi1 -ScriptBlock {Invoke-Expression "$scriptPath $argumentList"} -credential $vmcred
