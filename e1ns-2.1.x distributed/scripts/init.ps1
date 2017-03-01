cb5afd6bc008376c1f183d0c13e2e666f2180b87
JanHendrikDolling <github@sp.dollares.de>
2017-03-01 12:07:08 +0100

    tae

--- e1ns-2.1.x distributed/scripts/init.ps1
+++ e1ns-2.1.x distributed/scripts/init.ps1
@@ -6,7 +6,7 @@
     [string]$vmUi2 = "vm-ui2",
     [string]$vmRep = "vm-rep",
     [string]$vmIndex = "vm-index",
-    [string]$adminPw = "plato"
+    [SecureString]$adminPw = "plato"
  )
 
 $FolderName = "C:\share"
@@ -26,7 +26,7 @@ $scio_db = "https://www.hidrive.strato.com/wget/LBLuUBRG"
 $e1ns = "https://www.hidrive.strato.com/wget/s7LO0GH0" 
 $e1ns_config = "https://www.hidrive.strato.com/wget/HpLO0mAk"
 $setup_script = "https://raw.githubusercontent.com/JanHendrikDolling/e1ns/master/e1ns-2.1.x%20distributed/scripts/setup.ps1"
-$invoke_script = "https://raw.githubusercontent.com/JanHendrikDolling/e1ns/master/e1ns-2.1.x%20distributed/scripts/invoke.ps1"
+
 
 Invoke-WebRequest -Method Get -Uri $scio_server -OutFile $SetupFolderName\setup_scio_server.exe ;
 Invoke-WebRequest -Method Get -Uri $scio_client -OutFile $SetupFolderName\setup_scio_client.exe ;
@@ -34,7 +34,6 @@ Invoke-WebRequest -Method Get -Uri $scio_db -OutFile $SetupFolderName\db.zip ;
 Invoke-WebRequest -Method Get -Uri $e1ns -OutFile $SetupFolderName\setup_e1ns.exe ;
 Invoke-WebRequest -Method Get -Uri $e1ns_config -OutFile $SetupFolderName\e1ns_config.yml ;
 Invoke-WebRequest -Method Get -Uri $setup_script -OutFile $SetupFolderName\setup.ps1 ;
-Invoke-WebRequest -Method Get -Uri $invoke_script -OutFile $SetupFolderName\invoke.ps1 ;
 
 # create share
 New-SmbShare –Name $ShareName –Path $FolderName –FullAccess Everyone 
@@ -55,38 +54,38 @@ $vmcred = new-object -typename System.Management.Automation.PSCredential -argume
 
 
 # init script items
+$localScriptPath = $FolderName + "\setup"
 $scriptPath = "\\" + $vmDB + "\data\setup"
-$setupScriptPath = $scriptPath + "\setup.ps1"
 $items = @()
 
 $obj = New-Object System.Object
 $obj | Add-Member -type NoteProperty -name server -value $vmLB
-$obj | Add-Member -type NoteProperty -name setupCommand -value "$setupScriptPath -vmDB $vmDB -role 'lb'"
+$obj | Add-Member -type NoteProperty -name role -value "lb"
 $items += $obj 
 
 $obj = New-Object System.Object
 $obj | Add-Member -type NoteProperty -name server -value $vmUi1
-$obj | Add-Member -type NoteProperty -name setupCommand -value "$setupScriptPath -vmDB $vmDB -role 'ui'"
+$obj | Add-Member -type NoteProperty -name role -value "ui"
 $items += $obj 
 
 $obj = New-Object System.Object
 $obj | Add-Member -type NoteProperty -name server -value $vmUi2
-$obj | Add-Member -type NoteProperty -name setupCommand -value "$setupScriptPath -vmDB $vmDB -role 'ui'"
+$obj | Add-Member -type NoteProperty -name role -value "ui"
 $items += $obj 
 
 $obj = New-Object System.Object
 $obj | Add-Member -type NoteProperty -name server -value $vmDB
-$obj | Add-Member -type NoteProperty -name setupCommand -value "$setupScriptPath -vmDB $vmDB -role 'db'"
+$obj | Add-Member -type NoteProperty -name role -value "db"
 $items += $obj 
 
 $obj = New-Object System.Object
 $obj | Add-Member -type NoteProperty -name server -value $vmRep
-$obj | Add-Member -type NoteProperty -name setupCommand -value "$setupScriptPath -vmDB $vmDB -role 'rep'"
+$obj | Add-Member -type NoteProperty -name role -value "rep"
 $items += $obj 
 
 $obj = New-Object System.Object
 $obj | Add-Member -type NoteProperty -name server -value $vmIndex
-$obj | Add-Member -type NoteProperty -name setupCommand -value "$setupScriptPath -vmDB $vmDB -role 'index'"
+$obj | Add-Member -type NoteProperty -name role -value "index"
 $items += $obj 
 
 
@@ -94,14 +93,25 @@ $items += $obj
 workflow parallelCheckServer {
     param (
         $items,
+        $dbServer,
+        $localScriptPath,
         [SecureString]$vmcred
     )
     foreach -parallel($item in $items)
     {
-        Invoke-Expression -Command "$scriptPath invoke.ps1 -server " + $item.server + " -command " + $item.setupCommand + " -vmcred $vmcred"
+        $server = $item.server
+        $setupScriptPath = $localScriptPath + "\setup.ps1"
+
+        #arguments for script
+        $argumentlist = @()
+        $argumentlist += $dbServer
+        $argumentlist += $item.role
+
+        #invoce local script on remoute computer
+        Invoke-Command -ComputerName $server -credential $vmcred -FilePath $setupScriptPath -argumentlist $argumentlist
     }
 }
-parallelCheckServer -items $items -vmcred $vmcred
+parallelCheckServer -items $items -dbServer $vmDB -localScriptPath $localScriptPath -vmcred $vmcred
 
 
 # configure software
