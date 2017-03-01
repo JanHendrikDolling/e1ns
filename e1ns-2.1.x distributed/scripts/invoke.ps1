8164e43bd50bad62a68cd134bca26bb5dad86d9d
JanHendrikDolling <github@sp.dollares.de>
2017-03-01 12:05:50 +0100

    tae

--- e1ns-2.1.x distributed/scripts/invoke.ps1
+++
@@ -1,7 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)] [string] $server,
-    [Parameter(Mandatory=$true)] [string] $command,
-    [Parameter(Mandatory=$true)] [SecureString] $vmcred
-)
-
-Invoke-Command -ComputerName $server -ScriptBlock {Invoke-Expression "$command"} -credential $vmcred
